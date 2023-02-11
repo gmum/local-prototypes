Fixes after test runs for masking

main.py
@@ -1,9 +1,7 @@
 import os
 import shutil
 
-import torch
 import torch.utils.data
-# import torch.utils.data.distributed
 import torchvision.transforms as transforms
 import torchvision.datasets as datasets
 
@@ -17,7 +15,7 @@ import train_and_test as tnt
 import save
 from log import create_logger
 from preprocess import mean, std, preprocess_input_function
-from settings import NEPTUNE_API_TOKEN
+from settings import NEPTUNE_API_TOKEN, max_num_cycles, masking_random_prob
 import neptune.new as neptune
 
 parser = argparse.ArgumentParser()
@@ -28,7 +26,6 @@ parser.add_argument('--masking_type', type=str, default='none')
 
 args = parser.parse_args()
 os.environ['CUDA_VISIBLE_DEVICES'] = args.gpuid[0]
-print(os.environ['CUDA_VISIBLE_DEVICES'])
 
 # book keeping namings and code
 from settings import base_architecture, img_size, prototype_shape, num_classes, \
@@ -97,11 +94,6 @@ test_loader = torch.utils.data.DataLoader(
     num_workers=4, pin_memory=False)
 
 # we should look into distributed sampler more carefully at torch.utils.data.distributed.DistributedSampler(train_dataset)
-log('training set size: {0}'.format(len(train_loader.dataset)))
-log('push set size: {0}'.format(len(train_push_loader.dataset)))
-log('test set size: {0}'.format(len(test_loader.dataset)))
-log('batch size: {0}'.format(train_batch_size))
-
 # construct the model
 ppnet = model.construct_PPNet(base_architecture=base_architecture,
                               pretrained=True, img_size=img_size,
@@ -159,6 +151,7 @@ if isinstance(NEPTUNE_API_TOKEN, str) and len(NEPTUNE_API_TOKEN) > 0:
         "masking_type": args.masking_type,
         "num_train_epochs": num_train_epochs,
         "num_warm_epochs": num_warm_epochs,
+        "max_num_cycles": max_num_cycles,
         "coefs": coefs,
         "joint_optimizer_lrs": joint_optimizer_lrs,
         "joint_optimizer_step_size": joint_lr_step_size,
@@ -175,23 +168,26 @@ if isinstance(NEPTUNE_API_TOKEN, str) and len(NEPTUNE_API_TOKEN) > 0:
         "test_batch_size": test_batch_size,
         "train_push_batch_size": train_push_batch_size,
         "push_start": push_start,
-        "push_epochs": push_epochs
+        "push_epochs": push_epochs,
+        'masking_random_prob': masking_random_prob,
     }
     neptune_run["parameters"] = params
 else:
     neptune_run = None
 
-# train the model
+log('training set size: {0}'.format(len(train_loader.dataset)))
+log('push set size: {0}'.format(len(train_push_loader.dataset)))
+log('test set size: {0}'.format(len(test_loader.dataset)))
+log('batch size: {0}'.format(train_batch_size))
+
 log('start training')
-import copy
 
 max_accu_no_push = 0.0
 max_accu_push = 0.0
 max_accu_finetune = 0.0
+n_cycle = 0
 
 for epoch in range(num_train_epochs):
-    log('epoch: \t{0}'.format(epoch))
-
     if epoch < num_warm_epochs:
         tnt.warm_only(model=ppnet_multi, log=log)
         train_accu, converged, metrics = tnt.train(model=ppnet_multi, dataloader=train_loader, optimizer=warm_optimizer,
@@ -206,7 +202,7 @@ for epoch in range(num_train_epochs):
                                                    masking_type=args.masking_type, neptune_run=neptune_run)
     if neptune_run is not None:
         neptune_run["train/epoch/accuracy"].append(train_accu)
-        neptune_run["train/epoch/stage"].append(1.0)
+        neptune_run["train/epoch/stage"].append(0.0 if epoch < num_warm_epochs else 1.0)
         neptune_run["train/epoch/converged"].append(float(int(converged)))
         for key, val in metrics.items():
             neptune_run[f"train/epoch/{key}"].append(float(val))
@@ -215,18 +211,19 @@ for epoch in range(num_train_epochs):
                                 class_specific=class_specific, log=log, masking_type=args.masking_type,
                                 neptune_run=neptune_run)
 
-    if accu > max_accu_no_push:
-        save.save_model_w_condition(model=ppnet, model_dir=model_dir, model_name='nopush_best', accu=accu,
-                                    target_accu=0.10, log=log)
-        max_accu_no_push = accu
-
     if neptune_run is not None:
         neptune_run["test/epoch/accuracy"].append(accu)
         for key, val in metrics.items():
             neptune_run[f"test/epoch/{key}"].append(float(val))
 
+    if accu > max_accu_no_push:
+        log(f"Cycle {n_cycle} - new best test accuracy: {accu:.2f}")
+        save.save_model_w_condition(model=ppnet, model_dir=model_dir, model_name='nopush_best', accu=accu,
+                                    target_accu=0.10, log=log, cycle=n_cycle)
+        max_accu_no_push = accu
+
     save.save_model_w_condition(model=ppnet, model_dir=model_dir, model_name='nopush_last', accu=accu,
-                                target_accu=0.10, log=log)
+                                target_accu=0.10, log=log, cycle=n_cycle)
 
     if epoch >= push_start and epoch in push_epochs:
         push.push_prototypes(
@@ -248,11 +245,11 @@ for epoch in range(num_train_epochs):
 
         if accu > max_accu_push:
             save.save_model_w_condition(model=ppnet, model_dir=model_dir, model_name='push_best', accu=accu,
-                                        target_accu=0.10, log=log)
+                                        target_accu=0.10, log=log, cycle=n_cycle)
             max_accu_push = accu
 
         save.save_model_w_condition(model=ppnet, model_dir=model_dir, model_name='push_last', accu=accu,
-                                    target_accu=0.10, log=log)
+                                    target_accu=0.10, log=log, cycle=n_cycle)
 
         if prototype_activation_function != 'linear':
             tnt.last_only(model=ppnet_multi, log=log)
@@ -276,27 +273,37 @@ for epoch in range(num_train_epochs):
                                             class_specific=class_specific, log=log, masking_type=args.masking_type,
                                             neptune_run=neptune_run)
 
-                if accu > max_accu_finetune:
-                    save.save_model_w_condition(model=ppnet, model_dir=model_dir, model_name='push_finetune_best',
-                                                accu=accu, target_accu=0.10, log=log)
-                    max_accu_finetune = accu
-                save.save_model_w_condition(model=ppnet, model_dir=model_dir, model_name='push_finetune_last',
-                                            accu=accu, target_accu=0.10, log=log)
-
                 if neptune_run is not None:
                     neptune_run["test/epoch/accuracy"].append(accu)
                     for key, val in metrics.items():
                         neptune_run[f"test/epoch/{key}"].append(float(val))
 
+            if accu > max_accu_finetune:
+                save.save_model_w_condition(model=ppnet, model_dir=model_dir, model_name='push_finetune_best',
+                                            accu=accu, target_accu=0.10, log=log, cycle=n_cycle)
+                max_accu_finetune = accu
+            save.save_model_w_condition(model=ppnet, model_dir=model_dir, model_name='push_finetune_last',
+                                        accu=accu, target_accu=0.10, log=log, cycle=n_cycle)
+
         if train_accu > 0.99 and converged and epoch > 20:
             print("EARLY STOPPING")
             break
 
+        # reset metrics after each cycle
+        max_accu_no_push = 0.0
+        max_accu_push = 0.0
+        max_accu_finetune = 0.0
+        n_cycle += 1
+
+        if n_cycle > max_num_cycles:
+            print("REACHED MAXIMUM NUMBER OF CYCLES ({})".format(max_num_cycles))
+            break
+
 print()
 print(f'{args.experiment_run} ACCURACIES: ')
-print("nopush: {:4.f}".format(max_accu_no_push))
-print("push: {:4.f}".format(max_accu_push))
-print("push_finetune: {:4.f}".format(max_accu_finetune))
+print("nopush: {:.4f}".format(max_accu_no_push))
+print("push: {:.4f}".format(max_accu_push))
+print("push_finetune: {:.4f}".format(max_accu_finetune))
 print()
 
 logclose()

run_pruning.py
@@ -44,7 +44,7 @@ shutil.copy(src=os.path.join(os.getcwd(), __file__), dst=model_dir)
 
 log, logclose = create_logger(log_filename=os.path.join(model_dir, 'prune.log'))
 
-ppnet = torch.load(original_model_dir + original_model_name)
+ppnet = torch.load(os.path.join(original_model_dir, original_model_name))
 ppnet = ppnet.cuda()
 ppnet_multi = torch.nn.DataParallel(ppnet)
 class_specific = True
@@ -163,12 +163,12 @@ if optimize_last_layer:
         if neptune_run is not None:
             neptune_run["test/epoch/accuracy"].append(accu)
 
-        if accu > best_accu:
-            save.save_model_w_condition(model=ppnet, model_dir=model_dir,
-                                        model_name='prune_best',
-                                        accu=accu,
-                                        target_accu=0.10, log=log)
-            best_accu = accu
+        # if accu > best_accu:
+            # save.save_model_w_condition(model=ppnet, model_dir=model_dir,
+                                        # model_name='prune_best',
+                                        # accu=accu,
+                                        # target_accu=0.10, log=log)
+            # best_accu = accu
 
     save.save_model_w_condition(model=ppnet, model_dir=model_dir,
                                 model_name='prune_last',

save.py
@@ -2,11 +2,13 @@ import os
 import torch
 
 
-def save_model_w_condition(model, model_dir, model_name, accu, target_accu, log=print):
+def save_model_w_condition(model, model_dir, model_name, accu, target_accu, log=print, cycle=None):
     '''
     model: this is not the multigpu model
     '''
     if accu > target_accu:
-        log('\tabove {0:.2f}%'.format(target_accu * 100))
         # torch.save(obj=model.state_dict(), f=os.path.join(model_dir, (model_name + '{0:.4f}.pth').format(accu)))
-        torch.save(obj=model, f=os.path.join(model_dir, model_name))
+        torch.save(obj=model, f=os.path.join(model_dir, f'{model_name}.pth'))
+
+        if cycle is not None:
+            torch.save(obj=model, f=os.path.join(model_dir, f'{model_name}_cycle_{cycle}.pth'))

train_and_test.py
@@ -3,6 +3,7 @@ import torch
 import numpy as np
 
 from helpers import list_of_distances
+from settings import masking_random_prob
 
 
 def _train_or_test(model, dataloader, optimizer=None, class_specific=True, use_l1_mask=True,
@@ -23,6 +24,7 @@ def _train_or_test(model, dataloader, optimizer=None, class_specific=True, use_l
     # separation cost is meaningful only for class_specific
     total_separation_cost = 0
     total_avg_separation_cost = 0
+    total_sim_diff_loss = 0.0
 
     for i, (image, label) in enumerate(dataloader):
         input = image.cuda()
@@ -48,9 +50,8 @@ def _train_or_test(model, dataloader, optimizer=None, class_specific=True, use_l
                 # torch.Size([2000, 200])
 
                 if masking_type == 'random':
-                    RANDOM_PROB = 0.8
                     random_mask = (torch.cuda.FloatTensor(all_similarities.shape[0], 1, all_similarities.shape[-1],
-                                                          all_similarities.shape[-1]).uniform_() > RANDOM_PROB).float()
+                                                          all_similarities.shape[-1]).uniform_() > masking_random_prob).float()
                     random_mask_img = torch.nn.functional.interpolate(random_mask,
                                                                       size=(input.shape[-1], input.shape[-1])).long()
                     new_input = input * random_mask_img
@@ -138,6 +139,7 @@ def _train_or_test(model, dataloader, optimizer=None, class_specific=True, use_l
             total_cluster_cost += cluster_cost.item()
             total_separation_cost += separation_cost.item()
             total_avg_separation_cost += avg_separation_cost.item()
+            total_sim_diff_loss += sim_diff_loss.item() if torch.is_tensor(sim_diff_loss) else 0
 
         # compute gradient and do SGD step
         if is_train:
@@ -176,24 +178,24 @@ def _train_or_test(model, dataloader, optimizer=None, class_specific=True, use_l
 
     end = time.time()
 
-    log('\ttime: \t{0}'.format(end - start))
-    if is_train:
-        log('t\loss: \t{0}'.format(total_loss / n_batches))
-    log('\tcross ent: \t{0}'.format(total_cross_entropy / n_batches))
-    log('\tcluster: \t{0}'.format(total_cluster_cost / n_batches))
-    if class_specific:
-        log('\tseparation:\t{0}'.format(total_separation_cost / n_batches))
-        log('\tavg separation:\t{0}'.format(total_avg_separation_cost / n_batches))
-    log('\taccu: \t\t{0}%'.format(n_correct / n_examples * 100))
-    l1 = model.module.last_layer.weight.norm(p=1).item()
-    log('\tl1: \t\t{0}'.format(l1))
+    # log('\ttime: \t{0}'.format(end - start))
+    # if is_train:
+        # log('t\loss: \t{0}'.format(total_loss / n_batches))
+    # log('\tcross ent: \t{0}'.format(total_cross_entropy / n_batches))
+    # log('\tcluster: \t{0}'.format(total_cluster_cost / n_batches))
+    # if class_specific:
+        # log('\tseparation:\t{0}'.format(total_separation_cost / n_batches))
+        # log('\tavg separation:\t{0}'.format(total_avg_separation_cost / n_batches))
+    # log('\taccu: \t\t{0}%'.format(n_correct / n_examples * 100))
+    # l1 = model.module.last_layer.weight.norm(p=1).item()
+    # log('\tl1: \t\t{0}'.format(l1))
     p = model.module.prototype_vectors.view(model.module.num_prototypes, -1).cpu()
     with torch.no_grad():
         p_avg_pair_dist = torch.mean(list_of_distances(p, p)).item()
-    log('\tp dist pair: \t{0}'.format(p_avg_pair_dist))
-    if isinstance(sim_diff_loss, torch.Tensor):
-        sim_diff_loss = sim_diff_loss.item()
-    log('\tsim diff:  \t{0}'.format(sim_diff_loss))
+    # log('\tp dist pair: \t{0}'.format(p_avg_pair_dist))
+    # if isinstance(sim_diff_loss, torch.Tensor):
+        # sim_diff_loss = sim_diff_loss.item()
+    # log('\tsim diff:  \t{0}'.format(sim_diff_loss))
 
     converged = total_cluster_cost < total_separation_cost
 
@@ -204,8 +206,11 @@ def _train_or_test(model, dataloader, optimizer=None, class_specific=True, use_l
         'avg_separation': total_avg_separation_cost,
         'l1': l1,
         'p_avg_pair_dist': p_avg_pair_dist,
-        'sim_diff_loss': sim_diff_loss
     }
+
+    if masking_type != 'none':
+        metrics['sim_diff_loss'] = total_sim_diff_loss / n_batches
+
     if is_train:
         metrics['loss'] = total_loss / n_batches
 
@@ -215,8 +220,6 @@ def _train_or_test(model, dataloader, optimizer=None, class_specific=True, use_l
 def train(model, dataloader, optimizer, class_specific=False, coefs=None, log=print, masking_type='none',
           neptune_run=None):
     assert(optimizer is not None)
-    
-    log('\ttrain')
     model.train()
     return _train_or_test(model=model, dataloader=dataloader, optimizer=optimizer,
                           class_specific=class_specific, coefs=coefs, log=log, masking_type=masking_type,
@@ -224,7 +227,6 @@ def train(model, dataloader, optimizer, class_specific=False, coefs=None, log=pr
 
 
 def test(model, dataloader, class_specific=False, log=print, masking_type='none', neptune_run=None):
-    log('\ttest')
     model.eval()
     return _train_or_test(model=model, dataloader=dataloader, optimizer=None,
                           class_specific=class_specific, log=log, masking_type=masking_type, neptune_run=neptune_run)